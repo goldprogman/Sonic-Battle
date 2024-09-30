f5130e745adb079e8777a5a32475239165ba6e92
Alison <29799190+Autoblockinator@users.noreply.github.com>
2024-09-30 21:01:53 +0100

    Added copyright notices.

--- ai/behaviour_tree/behaviour_tree.gd
+++ ai/behaviour_tree/behaviour_tree.gd
@@ -1,3 +1,11 @@
+# Copyright 2024 Alison Janvier
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the “Software”), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED “AS IS”, WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 ## The root node of a behaviour tree.
 extends Node
 class_name BehaviourTree
@@ -9,7 +17,7 @@ signal reevaluating
 
 var active_path: Array[BehaviourTreeNode] = []
 var root_flow_controller: BehaviourTreeFlowController = null
-var references := {'root': self}
+var blackboard := {'root': self}
 
 
 func evaluate():
@@ -51,7 +59,7 @@ func _ready():
 		'Tree has no valid root flow control node.'
 	)
 
-	root_flow_controller.start(references)
+	root_flow_controller.start(blackboard)
 	evaluate()
 	return
 
